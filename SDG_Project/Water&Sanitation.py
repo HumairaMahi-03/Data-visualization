75ebc27cea5cd7ae95cbac79618e5f2714febf42
HumairaMahi-03 <humairamumtahinmahi@gmail.com>
2026-09-07 13:51:08 +0600

    new projects

--- SDG_Project/Water&Sanitation.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt;
-
-contribution =['NGO Contribution','Safe Water Access','Sanitation Facilities','Hygiene Awareness']
-revenue=['15','35','30','20']
-plt.pie(revenue,labels=contribution,autopct='%1.1f%%',colors=['skyBlue','Lightgreen','lightblue','gold'])
-plt.title('SDG 6 Achievements & NGO Contribution in Bangladesh')
-plt.savefig('Water&Sanitation.png',dpi=300,bbox_inches='tight')
-plt.show()
